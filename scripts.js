752478ec279815d3142903662c3b332548974958
Jason Borer <borerjason@gmail.com>
2017-05-18 22:17:18 -0700

    api is working

--- scripts.js
+++ scripts.js
@@ -2,13 +2,49 @@ $(document).ready(function(){
 	$(".search").click(moveSearchBar);
 	$(".fa-search").click(moveSearchBar);
 
+	$(".fa-search").click(getResults);
+
 });
 
-var api_ex = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
-var api_endpt = "https://en.wikipedia.org/w/api.php";
+function getResults(){
+	var api_endpt = "http://en.wikipedia.org/w/api.php";
+	$.ajax({
+		url: api_endpt,
+		data: { action: 'query', list: 'search', srsearch: $("input[name=search]").val(), format: 'json' }, 
+		dataType: "jsonp",
+		type: "GET",
+		error: function(){
+			console.log("could not receive data");
+		},
+		success: printTitles
+	});
+	
+}
 
+function printTitles(data){
+	var arrSearch = data.query.search;
+	arrSearch.forEach(function(item){
+		console.log(item.title);
+		var snip = item.snippet.split('.');
+		console.log(snip[0]+ ".");
+	});
+}
+
+function printSnippet(data){
+	var arrSSearch = data.query.search;
+	arrSSearch.forEach(function(item){
+		var snip = item.snippet.split('.');
+		console.log(snip[0]+ ".");
+	})
+}
+
+function printData(data){
+	console.log(data.query.search[0].title);
+	console.log(data.query.search[0].snippet);
+}  
 
 function moveSearchBar(){
 	$("form").css("margin-top","0");
 }
+
 
