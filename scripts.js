9fe95251e6486e3e4bf606612745d4744947e99a
Jason Borer <borerjason@gmail.com>
2017-05-20 16:14:14 -0700

    final version for first go-around - submit to fcc

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-	
-	$(".search").click(moveSearchBar);
 	$(".fa-search").click(moveSearchBar);
 	$(".fa-search").click(getResults);
-	
-	// keyboard - institute after get drop down working
-	// $("input").keypress(function(event){
-    
-
+	$("input[name=search]").keypress(function(event){
+		var key = event.which;
+		if(key === 13){
+			$(".fa-search").click();
+			return false;
+		}
+	});
+	// clear the results divs when click the clear button
 	$(".fa-times-circle").click(function(){
 		$(".results").empty();
 		$('input[type=text]').val("");
 	});
-
-    
 });
-//https://www.mediawiki.org/wiki/API:Opensearch
 
+//retrieve data from wiki API
+//https://www.mediawiki.org/wiki/API:Opensearch
 function getResults(){
 	var api_endpt = "http://en.wikipedia.org/w/api.php";
 	$.ajax({
@@ -35,18 +35,9 @@ function printInfo(data){
 	$(".results").empty();
 	for(var i = 0; i < 10; i++){
 		$(".results").append("<a href ='" + data[3][i] + "' target = '_blank' class = 'box'  > <div class = 'page'>" + "<h4>" + data[1][i] + "</h4> <br> <p>" + data[2][i] + "</p> </div> </a>");	
-	};
+	};	
 }
 
 function moveSearchBar(){
 	$("form").css("margin-top", "10px");
 }
-
-
-
-// some learnings http://codepen.io/codinger/pen/adwErq
-/*
-var txtTitle = $("<h4></h4>").html(data[1][i]);
-		var txt = $("<p></p>").html(data[2][i]);
-		$(".results").append("<div class = 'page'>" + txtTitle).append(txt + "</div>");
-*/
